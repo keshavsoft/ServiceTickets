7f1f19741bcbb30b28bfc1775b0d577becc9ecd9
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-01-04 14:13:35 +0530

    SREENIVAS

--- common/Jwt/ForLogin/Users.js
+++ common/Jwt/ForLogin/Users.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import PackageJson from '../../../package.json'  assert { type: 'json' };
 
 let CommonEnvVariable = "KS_TOKEN_FORUSERS";
-let CommonTokenName = PackageJson.Keshavsoft.ForNodejs.Authantication.Forjwt.AdminKey;
+let CommonTokenName = PackageJson.Keshavsoft.ForNodejs.Authantication.Forjwt.UserKey;
 
 let CreateToken = ({ InUserId }) => {
     if (CommonEnvVariable in process.env === false) {
@@ -23,7 +23,7 @@ let VerifyToken = (req, res, next) => {
     };
 
     let LocalToken = process.env[CommonEnvVariable];
-    let LocalTokenFromCookie = req.cookies.KToken;
+    let LocalTokenFromCookie = req.cookies[CommonTokenName];
 
     try {
         var decoded = jwt.verify(LocalTokenFromCookie, LocalToken);
